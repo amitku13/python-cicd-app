6d233d3094c77d1ade69e7211aa79a04b56dab2e
mango <amit@gmail.com>
2026-02-14 16:39:48 +0530

    Add greet function back

--- app/main.py
+++ app/main.py
@@ -3,10 +3,13 @@ import os
 
 app = Flask(__name__)
 
+def greet(env):
+    return f"Hello from {env} environment!"
+
 @app.route("/")
 def home():
     env = os.getenv("ENVIRONMENT", "local")
-    return f"Hello from {env} environment!"
+    return greet(env)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
